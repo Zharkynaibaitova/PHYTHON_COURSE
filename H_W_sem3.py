f884e647cfe38040d549c02b7db6512e0e9040e8
Zharkyn <zharkynaibaitova@gmail.com>
2023-06-28 21:29:09 +0600

    домашнии работа к семинару 4

--- H_W_sem3.py
+++ H_W_sem3.py
@@ -33,17 +33,17 @@
 # print(list_1[k])
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# keys = {'а', 'в', 'е', 'и', 'н', 'о', 'р', 'с', 'т'}
-# print(my_dict.fromkeys(keys, 'Пусто'))
-
-# 'д', 'к', 'л', 'м', 'п', 'у' 
-# 'б', 'г', 'ё', 'ь', 'я'
-# 'й', 'ы'
-# 'ж', 'з', 'х', 'ц', 'ч'
-# 'ш', 'э', 'ю'
-# 'ф', 'щ', 'ъ'
-
-my_dict = {}
-keys1 = {'д', 'к', 'л', 'м', 'п', 'у' }
-keys2 = {'б', 'г', 'ё', 'ь', 'я'}
-print(my_dict.fromkeys(keys1, 1)(keys2,2))
+# points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+# points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+# word = k.upper()  # переводим все буквы в верхний регистр
+# count = 0
+# for i in word:
+#     if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+#         for j in points_en:
+#             if i in points_en[j]:
+#                 count = count + j
+#     else:
+#         for j in points_en:
+#             if i in points_ru[j]:
+#                 count = count + j
+# print(count)
